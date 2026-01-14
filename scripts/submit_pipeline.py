d80a0d5133da43f81eb9bb1c2c07c3ec4f6e51b1
Pedro Rohem <110731287+pprohem@users.noreply.github.com>
2026-01-14 18:56:54 -0300

    chore: add pipeline compilation step in submit_pipeline.py

--- scripts/submit_pipeline.py
+++ scripts/submit_pipeline.py
@@ -1,6 +1,8 @@
 # scripts/submit_pipeline.py
 import argparse
 from google.cloud import aiplatform
+from kfp import compiler
+from pipelines.train_pipeline import pipeline
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--image-uri", required=True)
@@ -10,18 +12,26 @@ PROJECT_ID = "mlops-rohem"
 REGION = "us-central1"
 PIPELINE_ROOT = "gs://mlops-rohem-pipelines/pipelines"
 
+# 1️⃣ Compila a pipeline
+compiler.Compiler().compile(
+    pipeline_func=pipeline,
+    package_path="pipeline.json",
+)
+
+# 2️⃣ Inicializa o Vertex
 aiplatform.init(
     project=PROJECT_ID,
     location=REGION,
 )
 
+# 3️⃣ Submete a pipeline
 job = aiplatform.PipelineJob(
     display_name="cd4ml-train-pipeline",
     template_path="pipeline.json",
     pipeline_root=PIPELINE_ROOT,
     parameter_values={
         "image": args.image_uri
-    }
+    },
 )
 
 job.submit()
